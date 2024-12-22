0b9595fbe956c14192d9586157d19464898e48d8
ilhamme <icahyosw@gmail.com>
2024-12-22 12:37:56 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const themeToggle = document.getElementById('theme-toggle');
-
-themeToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    
-    // Change the theme toggle icon
-    const icon = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
-    themeToggle.innerHTML = `<span>${icon}</span>`;
-});
